857b615a2aaddf7830d1d2d3a352c03474dbde71
Milena Kari <milenakari@yahoo.com>
2018-11-27 02:53:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-   text.scan(/\b[A-Z]\S+\W\b/)
+   if(text.scan(/\A[A-Z]\S+\W\z/)
 end
 
 def valid_phone_number?(phone)
