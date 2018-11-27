8f25bee1aa2df6987c7bca9f903bc5e99921c313
Milena Kari <milenakari@yahoo.com>
2018-11-27 03:20:19 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if(word.scan(/\b[aeiouAEIOU]/) == [])
+    false
+  else
+    true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+   text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   if (text.scan(/\A[A-Z]/)!=[] && (text.scan(/[?!.]\z/))!=[])
+     true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+ if(phone.scan(/[0-9]/).length ==10)
+   true
+ else
+   false
+ end
 end
